47bd55e705e0e96b2da3ff79de3808cc466fa2ff
Anjali <anjaliiiiiiii333@gmail.com>
2026-07-24 14:47:56 +0530

    Process uploaded logs line by line

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import os
 
 app = Flask(__name__)
 
+# Folder to store uploaded log files
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/upload", methods=["POST"])
 def upload():
@@ -17,13 +20,19 @@ def upload():
     if file.filename == "":
         return "No file selected."
 
+    # Save uploaded file
     filepath = os.path.join(UPLOAD_FOLDER, file.filename)
     file.save(filepath)
 
-    with open(filepath, "r") as file:
-     content = file.read()
+    # Read the log file line by line
+    logs = []
 
-    return f"<pre>{content}</pre>"
+    with open(filepath, "r") as file:
+     for line in file:
+        logs.append(line.strip())
+
+    return "<pre>" + "\n".join(logs) + "</pre>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
